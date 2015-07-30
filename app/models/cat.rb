25aff7af18965afd1a71171fe270ff1f5d4a1efc
beniutek <bbeniutek@gmail.com>
2015-07-30 20:14:13 +0200

    added scopes to cats

--- app/models/cat.rb
+++ app/models/cat.rb
@@ -1,3 +1,4 @@
 class Cat < ActiveRecord::Base
-	
+	scope :top_cats, ->(limit = 10, time = Time.beginning_of_day) { where("uploaded_at >= ?",time).order(cuteness_score: :desc).limit limit }
+	scope :top_cats_all_time, ->(limit = 10) { order(cuteness_score: :desc).limit limit }
 end
