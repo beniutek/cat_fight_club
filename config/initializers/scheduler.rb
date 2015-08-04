23a2c97be78bae64bedea209c63bbf90575703f5
beniutek <bbeniutek@gmail.com>
2015-08-04 03:27:19 +0200

    more fixes

--- config/initializers/scheduler.rb
+++ config/initializers/scheduler.rb
@@ -4,10 +4,6 @@ require 'rufus/scheduler'
 s = Rufus::Scheduler.new
 
 s.cron '30 02 * * *' do	
-	clowder = Cat.search_for_cats
-
-	clowder.each do |bobcat|
-		Cat.create(thumb_image_of_the_cat: bobcat)
-	end
+	Cat.search_for_cats
 end
 
